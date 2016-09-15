28c05922b355d42fbdcaaf7d57969c3c9cb3d789
Albert Zasłonka <bercik156@gmail.com>
2016-09-15 02:44:21 +0200

    add completed?

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:title, :note, :deadline)
+    params.require(:task).permit(:title, :note, :deadline, :completed)
   end
 
 end
